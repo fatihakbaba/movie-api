e69cb8b69600aa741b499fa3119d3b52ff726604
Fatih Akbaba <fatihakbaba35@gmail.com>
2020-01-19 11:51:12 +0300

    api/directors POST Method

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 //const usersRouter = require('./routes/users');
 const movieRouter = require('./routes/movie');
+const directorRouter = require('./routes/director');
 
 const db = require('./helper/db')();
 
@@ -24,22 +25,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
-app.use('/api/movie', movieRouter)
+app.use('/api/movies', movieRouter);
+app.use('/api/directors', directorRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: { message: err.message, code: err.code }});
 });
 
 module.exports = app;
