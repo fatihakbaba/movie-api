12c2bf539892bfd4d5558303aa417ec52eab690d
Fatih Akbaba <fatihakbaba35@gmail.com>
2020-01-19 12:41:50 +0300

    Movie and Director validation

--- models/Movie.js
+++ models/Movie.js
@@ -4,12 +4,18 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: [25, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır.'],
+        minlength: [1, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.'],
     },
     category: String,
     country: String,
     year: Number,
-    imdb_score: Number,
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     director_id: Schema.Types.ObjectId,
     createdAt: {
         type: Date,
