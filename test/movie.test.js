104da7ef3b8c9828b1ea8a8d7abcdc2882ad3272
Fatih Akbaba <fatihakbaba35@gmail.com>
2020-01-25 14:43:12 +0300

    /api/movies [POST] Test

--- test/movie.test.js
+++ test/movie.test.js
@@ -23,12 +23,41 @@ describe('/api/movies tests ', () => {
     describe('/GET movies', () => {
         it('it should GET all the movies', (done) => {
             chai.request(server)
-            .get('/api/movies')
-            .set('x-access-token', token)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
+                .get('/api/movies')
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
                 done();
+            });
+        });
+    });
+
+    describe('/POST movie', () => {
+        it('it should POST a movie', (done) => {
+            const movie = {
+                title: 'Test',
+                category: 'Test Category',
+                country: 'Test Country',
+                year: 1950,
+                imdb_score: 8,
+                director_id: '5e2418359863bd38d08a75c7',
+            };
+
+            chai.request(server)
+                .post('/api/movies')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('director_id');
+                    done();
             });
         });
     });
